8ddff946ad2610043bed84bd6990ebae27962f2c
Noznoc <julia.conzon@mapbox.com>
2017-10-05 11:45:55 -0500

    cleaned code

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ mapboxgl.accessToken = 'pk.eyJ1IjoianVsY29ueiIsImEiOiJjajJjbWxieGswNDJxMzNvOHk4d
 
 const years = [{name: 'all'}, {name: '2016'}, {name: '2015'}, {name: '2014'}, {name: '2013'}, {name: '2012'}, {name: '2011'}, {name: '2010'}, {name: '2009'}, {name: '2008'}, {name: '2007'}, {name: '2006'}, {name: '2005'}, {name: '2004'}, {name: '2003'}];
 const types = [{name: 'all', value: 'all'}, {name: 'Break & Enter Commercial', value: 'Break and Enter Commercial'}, {name: 'Break & Enter Residential / Other', value: 'Break and Enter Residential/Other'}, {name: 'Mischief', value: 'Mischief'}, {name: 'Other Theft', value: 'Other Theft'}, {name: 'Theft from Vehicle', value: 'Theft from Vehicle'}, {name: 'Theft of Bicycle', value: 'Theft of Bicycle'}, {name: 'Theft of Vehicle', value: 'Theft of Vehicle'}, {name: 'Vehicle Collision or Pedestrian Struck (with Injury)', value: 'Vehicle Collision or Pedestrian Struck (with Injur'}, {name: 'Vehicle Collision or Pedestrian Struck (with Fatality)', value: 'Vehicle Collision or Pedestrian Struck (with Fatal'}];
+
+// remove this at some point and make script pull stops from layer... const stops = this.map.getPaintProperty(layer, 'fill-color');
 const stops = [[7500, 'rgba(253, 197, 190, 0.7)'],[15000, 'rgba(252, 162, 152, 0.7)'], [22500, 'rgba(251, 128, 114, 0.7)'], [30000, 'rgba(206, 105, 94, 0.7)'],[37500, 'rgba(137, 70, 63, 0.7)'],[45000, 'rgba(69, 35, 32, 0.7)']]
 
+// info for toggle options
 const options = [{
   name: 'Crime by Neighbourhood 2016',
   layer: 'vancouver-crime-nhoods',
@@ -17,13 +20,12 @@ const options = [{
   {name: 'All Crime 2016',
   layer: '2016',
   otherLayer: 'vancouver-crime-nhoods',
-  stops: [['all' , 'rgba(42, 38, 44, 1)']]
+  stops: []
 }]
 
 class Application extends React.Component {
   map;
   popupContainer;
-  data;
 
   constructor(props: Props) {
     super(props);
@@ -43,12 +45,11 @@ class Application extends React.Component {
       style: 'mapbox://styles/julconz/cj7rpgkw4e3yl2smwad8ddg7c',
       center: [-123.1846069, 49.265],
       zoom: 11.7,
-      maxZoom: 16,
-      minZoom: 11.7
+      maxZoom: 18,
+      minZoom: 11
     });
 
     this.popupContainer = document.createElement('div');
-
     const popup = new mapboxgl.Marker(this.popupContainer, {
       offset: [-122, 0]
     }).setLngLat([0,0]).addTo(this.map);
@@ -58,6 +59,15 @@ class Application extends React.Component {
       popup.setLngLat(e.lngLat);
       this.map.getCanvas().style.cursor = features.length ? 'pointer' : '';
       this.setPopups(features);
+
+      if (this.state.active.layer === 'vancouver-crime-nhoods'){
+        const features = this.map.queryRenderedFeatures(e.point, {layers: [this.state.active.layer]});
+        if (features.length){
+          document.getElementById("stats-choropleth").innerHTML = '<h2> Neighbourhood: ' + features[0].properties.NAME + '</h2><h2> Count: ' + features[0].properties.count + '</h2>'
+        }
+      } else {
+        document.getElementById("stats-choropleth").innerHTML = '';
+      }
     });
   }
 
@@ -65,7 +75,6 @@ class Application extends React.Component {
   handleChange(event) {
     // remove the layers that already exist
     this.map.setLayoutProperty(this.state.selectValues.year, 'visibility', 'none');
-    this.map.setLayoutProperty('2016-type', 'visibility', 'none');
 
     // get the values from the select tags
     let selectValues = this.state.selectValues;
@@ -77,9 +86,11 @@ class Application extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    this.map.setLayoutProperty('2016-type', 'visibility', 'none');
+
     this.map.setLayoutProperty('2016', 'visibility', 'none');
-    this.setFilter(this.state.selectValues); // input the selected values
+    
+    // input the selected values
+    this.setFilter(this.state.selectValues); 
     this.setStats(this.state.selectValues);
   }
 
@@ -87,19 +98,24 @@ class Application extends React.Component {
     let layer = this.state.active.layer;
     let otherLayer = this.state.active.otherLayer;
 
-    /*if (this.state.selectValues.year == '2016') {
-      document.getElementById('toggle').style.display = 'block';
-    }*/
-    if (layer === 'vancouver-crime-nhoods'){
+    /*if (layer === 'vancouver-crime-nhoods'){
       this.setLegend(layer);
-    }
+    }*/
+    
     this.map.setLayoutProperty(otherLayer, 'visibility', 'none');
     this.map.setLayoutProperty(layer, 'visibility', 'visible');
   }
 
   setFilter(values){
     var filter = [];
-    document.getElementById('toggle').style.display = 'none';
+
+    // hide legend & toggle if it is not 2016 data
+    if (values.year !== '2016'){
+      document.getElementById('legend-choropleth').style.display = 'none';
+      document.getElementById('toggle').style.display = 'none';
+      this.map.setLayoutProperty('vancouver-crime-nhoods', 'visibility', 'none');
+    }
+    
     // based on selection, output the following filter
     if (values.type !== 'all' && values.year !== 'all'){
       filter = ['all', ['==', 'TYPE', values.type], ['==', 'YEAR', parseInt(values.year)]];
@@ -124,6 +140,8 @@ class Application extends React.Component {
         ),
         this.popupContainer
       );
+    } else if (this.state.selectValues.layer === 'vancouver-crime-nhoods'){
+
     } else {
       this.popupContainer.innerHTML = '';
     }
@@ -131,44 +149,43 @@ class Application extends React.Component {
 
   setStats(values){
 
-    var features = this.map.queryRenderedFeatures({layers:[values.year]});
+    // need to figure out how to get all data from layer, not just what is rendered
+    //var features = this.map.queryRenderedFeatures({layers:[values.year]});
 
-    if (values.type == 'Vehicle Collision or Pedestrian Struck (with Injur') {
+    if (values.type === 'Vehicle Collision or Pedestrian Struck (with Injur') {
       values.type = 'Vehicle Collision or Pedestrian Struck (with Injury)';
     }
 
-    if (values.type == 'Vehicle Collision or Pedestrian Struck (with Fatal') {
+    if (values.type === 'Vehicle Collision or Pedestrian Struck (with Fatal') {
       values.type = 'Vehicle Collision or Pedestrian Struck (with Fatality)';
     }
 
-    if (values.year == 'all'){
+    if (values.year === 'all'){
       values.year = 'all years';
     }
 
-    //document.getElementById('stats').innerHTML =  features.length + " records of " + type + " crime in " + year;
     document.getElementById('stats').innerHTML =  values.type + " in " + values.year;
   }
 
-  setLegend(layer){
+  /*setLegend(layer){
     const stops = this.map.getPaintProperty(layer, 'fill-color');
-    //this.state.active.legend.stops = stops.stops;
-  }
+  }*/
 
   render() {
-   const { name, description, layer, stops } = this.state.active;
+   const { name, layer, stops } = this.state.active;
    const renderLegendKeys = (stop, i) => {
       console.log(stop)
-      //const stops = this.map.getPaintProperty(stop.layer, 'fill-color');
-      return (
-          <div key={i} className='txt-s'>
-            <span className='mr6 round-full w12 h12 inline-block align-middle' style={{ backgroundColor: stop[1] }} />
-            <span>{`${stop[0].toLocaleString()}`}</span>
-          </div>
-      );
+      if (stop.length){  
+        return (
+            <div key={i} className='txt-s'>
+              <span className='mr6 round-full w12 h12 inline-block align-middle' style={{ backgroundColor: stop[1] }} />
+              <span>{`${stop[0].toLocaleString()}`}</span>
+            </div>
+        );
+      }
     }
 
     const renderYears = (years, i) => {
-        
         return (
           <option key={i} value={years.name}>{years.name}</option>
         );
@@ -192,8 +209,9 @@ class Application extends React.Component {
     return (
       <div>
         <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
-        <a id="data" className="absolute bottom" href="http://data.vancouver.ca/datacatalogue/crime-data-details.htm?" target="_blank"><strong> source data </strong></a>
+        <a id="data" className="absolute bottom" rel="noopener noreferrer" href="http://data.vancouver.ca/datacatalogue/crime-data-details.htm?" target="_blank"><strong> source data </strong></a>
         <div id="legend" className="absolute">
+          <p className="txt-bold">Vancouver Crime Data (2003 - 2016)</p>
           <div className="filters">
             <form onClick={this.handleSubmit.bind(this)}>
               <div className="filter-container">
@@ -209,20 +227,20 @@ class Application extends React.Component {
                 </select>
               </div>
               <div className="filter-container" >
-                <label>Filter All Data</label>
+                <label>Filter Data</label>
                <input id="submit" type="submit" value="SUBMIT" />
               </div>
             </form>
             <div id="stats" className="txt-bold"></div>
           </div>
         </div>
-        <div id="toggle" className="legend toggle-group border border--2 shadow-darken10 z1">
+        <div id="toggle" className="legend toggle-group border border--1 shadow-darken10 z1">
           {options.map(renderToggle)}
         </div>
         <div id="legend-choropleth" className="legend absolute top left shadow-darken10 round wmax180">
-          <h2 className="txt-bold txt-s block">{name}</h2>
+          <h2 className="txt-bold block">{name}</h2>
           {stops.map(renderLegendKeys)}
-          <p className="txt-bold txt-s block" id="stats-choropleth"></p>
+          <p className="txt-bold block" id="stats-choropleth"></p>
         </div>
       </div>
     );
